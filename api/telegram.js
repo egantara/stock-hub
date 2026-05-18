a3c89602fe1b0735b8ba98384b56caf416051fca
egantara <egantarasatria@gmail.com>
2026-05-18 15:49:15 +0700

    debug bot

--- api/telegram.js
+++ api/telegram.js
@@ -2,25 +2,36 @@ import axios from 'axios'
 
 export default async function handler(req, res) {
 
-  const body = req.body
+  try {
 
-  const message =
-    body.message?.text
+    const body = req.body
 
-  const chatId =
-    body.message?.chat?.id
+    console.log(body)
 
-  if (!message) {
-    return res.status(200).send('ok')
-  }
+    const message =
+      body.message?.text
 
-  await axios.post(
-    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-    {
-      chat_id: chatId,
-      text: `Kamu bilang: ${message}`
+    const chatId =
+      body.message?.chat?.id
+
+    if (!message) {
+      return res.status(200).send('ok')
     }
-  )
 
-  return res.status(200).send('ok')
+    await axios.post(
+      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: `Kamu bilang: ${message}`
+      }
+    )
+
+    return res.status(200).send('ok')
+
+  } catch (err) {
+
+    console.log(err.response?.data || err.message)
+
+    return res.status(500).send('error')
+  }
 }
