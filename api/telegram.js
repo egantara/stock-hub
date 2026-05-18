736d9c27baf8f83e552309ebd5d51b05e4462eef
egantara <egantarasatria@gmail.com>
2026-05-18 15:34:25 +0700

    telegram webhook

--- api/telegram.js
+++ api/telegram.js
@@ -1,7 +1,26 @@
+import axios from 'axios'
+
 export default async function handler(req, res) {
 
-  return res.status(200).json({
-    message: 'BOT ACTIVE'
-  })
+  const body = req.body
 
+  const message =
+    body.message?.text
+
+  const chatId =
+    body.message?.chat?.id
+
+  if (!message) {
+    return res.status(200).send('ok')
+  }
+
+  await axios.post(
+    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+    {
+      chat_id: chatId,
+      text: `Kamu bilang: ${message}`
+    }
+  )
+
+  return res.status(200).send('ok')
 }
