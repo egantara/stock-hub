8e2a4ca5a6f093d06bc654358f4a2dda6d16b1e8
egantara <egantarasatria@gmail.com>
2026-05-19 11:29:00 +0700

    fix sync sheet env

--- api/services/sync-sheet.js
+++ api/services/sync-sheet.js
@@ -1,40 +1,65 @@
-import { google }
-from 'googleapis'
+import { GoogleSpreadsheet }
+from 'google-spreadsheet'
+
+import { JWT }
+from 'google-auth-library'
 
 export async function getSheetData() {
 
-  const auth =
-    new google.auth.GoogleAuth({
+  const credentials =
+    JSON.parse(
+      process.env.GOOGLE_SERVICE_ACCOUNT
+    )
 
-      credentials: {
-        client_email:
-          process.env.GOOGLE_CLIENT_EMAIL,
+  const serviceAccountAuth =
+    new JWT({
 
-        private_key:
-          process.env.GOOGLE_PRIVATE_KEY
-            .replace(/\\n/g, '\n')
-      },
+      email:
+        credentials.client_email,
+
+      key:
+        credentials.private_key,
 
       scopes: [
-        'https://www.googleapis.com/auth/spreadsheets.readonly'
+        'https://www.googleapis.com/auth/spreadsheets'
       ]
     })
 
-  const sheets =
-    google.sheets({
-      version: 'v4',
-      auth
-    })
+  const doc =
+    new GoogleSpreadsheet(
 
-  const response =
-    await sheets.spreadsheets.values.get({
+      process.env.GOOGLE_SHEET_ID,
 
-      spreadsheetId:
-        process.env.SPREADSHEET_ID,
+      serviceAccountAuth
+    )
 
-      range:
-        'Stock ALL!A:Z'
-    })
+  await doc.loadInfo()
 
-  return response.data.values
+  const sheet =
+    doc.sheetsByIndex[0]
+
+  const rows =
+    await sheet.getRows()
+
+  if (!rows.length) {
+    return []
+  }
+
+  const headers =
+    Object.keys(rows[0]._rawData)
+
+  const data = [
+    headers
+  ]
+
+  for (const row of rows) {
+
+    data.push(
+      headers.map(h =>
+        row.get(h)
+      )
+    )
+  }
+
+  return data
 }
