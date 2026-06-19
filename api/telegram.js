d3ff719523d54a8940d042b36cb6ec89d26386b1
egantara <egantarasatria@gmail.com>
2026-06-19 20:14:11 +0700

    add telegram api

--- api/telegram.js
+++ api/telegram.js
@@ -2,7 +2,21 @@ export default async function handler(
   req,
   res
 ) {
+
+  console.log(
+    "TELEGRAM UPDATE"
+  );
+
+  console.log(
+    JSON.stringify(
+      req.body,
+      null,
+      2
+    )
+  );
+
   return res.status(200).json({
     ok: true
   });
+
 }
